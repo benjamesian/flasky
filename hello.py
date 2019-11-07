9c340e057bf9d6a5b63466c38e58757ad62c7649
Ben Cook <808@holbertonschool.com>
2019-11-07 12:05:10 -0800

    use base template

--- hello.py
+++ hello.py
@@ -12,5 +12,13 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return f'<h1>Sorry, {e}</h1>', 500
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
