406070e1b45bce7e37fdf67cd854b50294d36d68
Ben Cook <808@holbertonschool.com>
2019-11-07 11:37:21 -0800

    first

---
+++ hello.py
@@ -0,0 +1,16 @@
+#!./venv/bin/python
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
